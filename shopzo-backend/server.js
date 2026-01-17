3c66fe279afd667136942b7f918f6cc88d1ea590
ajitkhetal <ajitkhetalofficial@gmail.com>
2026-01-17 19:18:17 +0530

    ops-panel

--- shopzo-backend/server.js
+++ shopzo-backend/server.js
@@ -10,10 +10,7 @@ import authMiddleware from "./middleware/auth.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
---
+++ shopzo-ops-panel/src/lib/api.ts
@@ -0,0 +1,9 @@
+const API_BASE_URL = "http://localhost:8000/api";
+
+ export const API_ENDPOINTS = {
+
+    LOGIN: `${API_BASE_URL}/auth/login`,
+    LOGOUT: `${API_BASE_URL}/auth/logout`,
+
+
+}
